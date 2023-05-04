41a74c3691565e8148643c6f7d1df2c6bcc00213
IsaoSaito <lsaoman240@gmail.com>
2023-05-04 20:32:11 +0900

    Terraform設定
    このTerraform設定には、以下の設定が含まれています。
    
    1. アップロード用と保存用のS3バケットを作成します。
    2. S3イベントを受信するSQSキューを作成します。
    3. アップロード用バケットの通知設定を構成し、オブジェクトが作成されるたびにSQSキューにメッセージを送信します。フィルタリング条件として、拡張子が`.png`のオブジェクトに対してのみイベントを送信するように設定しています。
    4. Lambda関数にアクセスするために必要なIAMロールを作成します。
    5. 画像の拡張子を変換するLambda関数を作成します。`filename`属性は、変換処理を実装した`lambda_function.zip`へのパスを指定してください。
    6. SQSキューをLambda関数のイベントソースとして設定します。これにより、SQSキューにメッセージが追加されるたびにLambda関数がトリガーされます。

--- main.tf
+++ main.tf
@@ -8,11 +8,33 @@ provider "aws" {
   skip_requesting_account_id  = true
 
   endpoints {
+    s3     = "http://localhost:4566"
+    sqs    = "http://localhost:4566"
     lambda = "http://localhost:4566"
-    iam    = "http://localhost:4566"
   }
 }
 
+resource "aws_s3_bucket" "upload_bucket" {
+  bucket = "my-upload-bucket"
+}
+
+resource "aws_s3_bucket" "save_bucket" {
+  bucket = "my-save-bucket"
+}
+
+resource "aws_sqs_queue" "s3_event_queue" {
+  name = "s3-event-queue"
+}
+
+resource "aws_s3_bucket_notification" "bucket_notification" {
+  bucket = aws_s3_bucket.upload_bucket.id
+
+  queue {
+    queue_arn     = aws_sqs_queue.s3_event_queue.arn
+    events        = ["s3:ObjectCreated:*"]
+    filter_suffix = ".png"
+  }
+}
 
 resource "aws_iam_role" "lambda_role" {
   name = "sample_lambda_role"
@@ -31,12 +53,28 @@ resource "aws_iam_role" "lambda_role" {
   })
 }
 
-resource "aws_lambda_function" "sample_lambda" {
-  function_name = "sample_lambda"
+resource "aws_lambda_function" "full_name_lambda" {
+  function_name = "full_name_lambda"
   handler       = "lambda_function.lambda_handler"
   runtime       = "python3.8"
 
   role = aws_iam_role.lambda_role.arn
 
-  filename = "lambda_function.zip"
+  filename = "full_name_lambda_function.zip"
+}
+
+
+resource "aws_lambda_function" "image_conversion_lambda" {
+  function_name = "image_conversion_lambda"
+  handler       = "lambda_function.lambda_handler"
+  runtime       = "python3.8"
+
+  role = aws_iam_role.lambda_role.arn
+
+  filename = "image_conversion_lambda_function.zip"
+}
+
+resource "aws_lambda_event_source_mapping" "sqs_lambda_mapping" {
+  event_source_arn = aws_sqs_queue.s3_event_queue.arn
+  function_name    = aws_lambda_function.image_conversion_lambda.arn
 }
