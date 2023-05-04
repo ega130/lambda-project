7f31fdf18737ea10bd9ff7ab7d2f7be75988a963
IsaoSaito <lsaoman240@gmail.com>
2023-05-04 17:22:12 +0900

    fix

--- main.tf
+++ main.tf
@@ -1,15 +1,18 @@
 provider "aws" {
+  profile                     = "localstack"
   region                      = "us-east-1"
-  access_key                  = "dummy"
-  secret_key                  = "dummy"
-  s3_force_path_style         = true
+  access_key                  = "test"
+  secret_key                  = "test"
+  s3_use_path_style           = true
   skip_credentials_validation = true
   skip_requesting_account_id  = true
 
   endpoints {
     lambda = "http://localhost:4566"
+    iam    = "http://localhost:4566"
   }
 }
+
 
 resource "aws_iam_role" "lambda_role" {
   name = "sample_lambda_role"
@@ -35,5 +38,5 @@ resource "aws_lambda_function" "sample_lambda" {
 
   role = aws_iam_role.lambda_role.arn
 
-  filename = "path/to/your/lambda_function.zip"
+  filename = "lambda_function.zip"
 }
