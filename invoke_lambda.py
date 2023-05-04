764395fda8baee036b9a0088186d3155be39eaac
IsaoSaito <lsaoman240@gmail.com>
2023-05-04 20:22:42 +0900

    eventとcontextの引数を使って、Lambda関数に情報を渡し、処理結果を返す
    eventにfirst_nameとlast_nameのキーを含むディクショナリを渡し、Lambda関数はフルネームを生成して返します

--- invoke_lambda.py
+++ invoke_lambda.py
@@ -1,4 +1,15 @@
 import boto3
+import json
+
+# 標準入力からfirst_nameとlast_nameを取得
+first_name = input("Enter first name: ")
+last_name = input("Enter last name: ")
+
+# eventディクショナリを作成
+event = {
+    "first_name": first_name,
+    "last_name": last_name
+}
 
 # LocalStackのエンドポイントを指定
 localstack_endpoint = 'http://localhost:4566'
@@ -7,12 +18,15 @@ localstack_endpoint = 'http://localhost:4566'
 # このクライアントは、LocalStackを使用してLambda関数を実行するために使用されます。
 lambda_client = boto3.client('lambda', endpoint_url=localstack_endpoint, region_name='us-east-1')
 
-# Lambda関数を実行
+# Lambda関数を実行してeventディクショナリを渡す
 # LocalStackは、Lambda関数の実行イベントを受信し、新しい環境を開始します。
 # Dockerコンテナ用のサービスエンドポイントが作成され、関数が実行されるエグゼキュータに割り当てられます。
 # Lambda関数用のDockerコンテナが作成され、設定が適用されます。
 # Lambda関数を実行するために必要なランタイムおよび依存関係がインストールされます。
-response = lambda_client.invoke(FunctionName='sample_lambda')
+response = lambda_client.invoke(
+    FunctionName='sample_lambda',
+    Payload=json.dumps(event)  # eventディクショナリをJSONに変換してPayload引数に渡す
+)
 
 # 実行が終了すると、invoke_lambda.py スクリプトの実行結果が出力されます。
 # これにより、AWS Lambda関数が正しく実行されたことが確認できます。
--- lambda_function.py
+++ lambda_function.py
@@ -1,4 +1,12 @@
 def lambda_handler(event, context):
-    message = "Hello from LocalStack Lambda!"
+    first_name = event.get('first_name', 'John')
+    last_name = event.get('last_name', 'Doe')
+    message = f"Hello {first_name} {last_name} from LocalStack Lambda!"
     print(message)
+
+    # contextオブジェクトからリクエストIDと実行時間を取得し、ログに記録
+    request_id = context.aws_request_id
+    remaining_time = context.get_remaining_time_in_millis()
+    print(f"Request ID: {request_id}, Remaining Time: {remaining_time}ms")
+
     return message
